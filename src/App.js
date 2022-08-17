9fc11096bb73e296ab4fa85ff7a53ceef927b403
Pabllot07 <pabllot07@outlook.com>
2022-08-17 18:14:01 -0300

    update form iniciated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 
@@ -8,7 +8,9 @@ import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'
 
+
 const App = () => {
+  const [currentId, setCurrentId] = useState(null)
   const classes = useStyles()
   const dispatch = useDispatch()
 
@@ -27,10 +29,10 @@ const App = () => {
         <Container>
           <Grid container justify='space-between' alignItems='stretch' spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId}  />
             </Grid>
           </Grid>
         </Container>
--- src/actions/posts.js
+++ src/actions/posts.js
@@ -8,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
     } catch (error) {
         console.log(error.message)
     }
-}
+};
 
 export const createPost = (newPost) => async (dispatch) => {
     try {
@@ -20,3 +20,13 @@ export const createPost = (newPost) => async (dispatch) => {
       console.log(error.message);
     }
   };
+
+  export const updatePost = (id, post) => async (dispatch) => {
+    try {
+      const { data } = await api.updatePost(id, post)
+
+      dispatch({ type: 'UPDATE', payload: data })
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
