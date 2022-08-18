d7c14b85e604d8c0833b116c38e5dfab9ebe2899
Pabllot07 <pabllot07@outlook.com>
2022-08-18 16:34:40 -0300

    like post implemented

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -17,6 +17,7 @@ export default makeStyles((theme) => ({
   fileInput: {
     width: '97%',
     margin: '10px 0',
+    overflow: 'hidden'
   },
   buttonSubmit: {
     marginBottom: 10,
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment'
 import { useDispatch } from 'react-redux'
 
 import useStyles from './styles'
-import { deletePost } from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
@@ -30,12 +30,12 @@ const Post = ({ post, setCurrentId }) => {
       </div>
         <Typography variant='body1' className={classes.title} gutterBottom>{post.title}</Typography>
       <CardContent>
-        <Typography variant='body1'  gutterBottom>{post.description}</Typography>
+        <Typography variant='body2' color='textSecondary' component='p'>{post.description}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
           <ThumbUpAltIcon fontSize='small' />
-          Like
+          &nbsp; Like &nbsp;
           {post.likeCount}
         </Button>
         <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
